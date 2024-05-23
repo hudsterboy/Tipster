57755ac845d1454b6e121370f0f948c4a334ef65
Eilidh Morone <e.silendt@gmail.com>
2024-05-23 15:28:18 +0100

    npm init add package.json and package-lock.json

---
+++ backend/src/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const host = "0.0.0.0";
+
+app.get("/", (req, res) => {
+  res.send("Hello World! -from server");
+});
+app.listen(port, host, () => {
+  console.log(`Server listening at http://${host}:${port}`);
+});
+
+module.exports = app;
